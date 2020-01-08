c3242ea7bc975ac39ae7f13e4c3effac5dbca853
filipe7rito <filipe7rito@gmail.com>
2020-01-08 23:42:55 +0000

    change dep

--- server.js
+++ server.js
@@ -3,13 +3,8 @@ var express = require("express");
 
 var app = express();
 
-//app.use(express.static(path.join(__dirname, "dist")));
-app.set("port", process.env.PORT || 8080);
-
 app.use(express.static(path.join(__dirname, "dist")));
-app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "dist", "index.html"));
-});
+app.set("port", process.env.PORT || 8080);
 
 var server = app.listen(app.get("port"), function() {
   console.log("listening on port ", server.address().port);
